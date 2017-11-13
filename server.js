7461f324e386f74d27e1dd9b4fd5f3fa8009e9e7
Glitch (fantastic-octave) <none>
2017-11-13 17:46:17 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 var strftime = require('strftime')
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -22,8 +14,9 @@ app.get("/:time", function (req, res) {
   } else {
     result = new Date (req.params.time);
   }
-
-  res.end(JSON.stringify({natural: strftime("%B %e, %Y", result), unixtime: result.valueOf() / 1000}));
+  
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify( {unix: result.valueOf() / 1000, natural: (result.valueOf()  ? strftime("%B %e, %Y", result) : null)} ));
 });
 
 
